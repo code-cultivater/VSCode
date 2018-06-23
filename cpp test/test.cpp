7d2001fb8d5447865f7e3a83c881ddab3df6919f
code-cultivator <352377811@qq.com>
2018-06-23 19:06:46 +0800

    1

---
+++ cpp test/test.cpp
@@ -0,0 +1,11 @@
+#include "iostream"
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    string str="i like freedom";
+    /* code */
+    cout << "Hello VSCode !!";
+    getline(cin,str);
+    return 0;
+}
